6948ba660c54de28a98c27b85728673ed2dbe5ba
Juan Azar <juan.azar@gmail.com>
2020-07-22 15:12:53 -0300

    Updated NavBar component

--- src/app/core/components/nav-bar/nav-bar.component.ts
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -8,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class NavBarComponent implements OnInit {
   linkId = 1;
   links = [ 
-    { id: 1, title: 'Clients', module: '/client' },
-    { id: 2, title: 'Domains', module: '/domain' }
+    {
+      linkId: 1, 
+      title: 'Clients',
+      sections: [
+        { title: "New Client", module: "/client/add" },
+        { title: "Client List", module: "/client/list" }
+      ]
+    },
+    {
+      linkId: 2, 
+      title: 'Domains',
+      sections: [
+        { title: "New Domain", module: "/domain/add" },
+        { title: "Domain List", module: "/domain/list" }
+      ]
+    }
   ]  
 
   constructor() { }
